009545e257ba859113d98a4d1de0615a3a62d826
Aleksei Moskvin <a.moskvin@propellerads.net>
2020-06-10 21:59:05 +0300

    change log file mode

--- log_analyzer.py
+++ log_analyzer.py
@@ -19,7 +19,7 @@ def main():
         logging.basicConfig(format='[%(asctime)s] %(levelname).1s %(message)s',
                             datefmt='%Y.%m.%d %H:%M:%S',
                             filename=config.get_value('LOG_FILE'),
-                            filemode='w+',
+                            filemode='a',
                             level=logging.INFO
                             )
         log_analyze(config)
